6bd0def5777898463bcd4bfe94697490bca494bf
thruston <663059+thruston@users.noreply.github.com>
2021-01-25 00:04:35 +0000

    Some parsing improvements

--- proust.py
+++ proust.py
@@ -8,9 +8,10 @@ this has to be in .gvimrc and you have to nop the macmenu key first.
     macmenu Edit.Font.Bigger key=<nop>
     imap <D-=> <ESC>:pyf ~/python-vim-proust/proust.py<CR>i
 
-The first line <nop>s the <Apple-=> key that is normally bound (by MacVim itself) to Edit.Font.Bigger
-The second line maps <Apple-=> (written as <D-=>) to the command sequence to run this filter.
-  
+The first line <nop>s the <Apple-=> key that is normally bound (by MacVim
+itself) to Edit.Font.Bigger.  The second line maps <Apple-=> (written as <D-=>)
+to the command sequence to run this filter.
+
   <ESC>                                   come out of insert mode
   :pyf ~/python-vim-proust.proust.py<CR>  run this filter
   i                                       go back into insert mode
@@ -18,10 +19,39 @@ The second line maps <Apple-=> (written as <D-=>) to the command sequence to run
 The filter is written to work with Python 2 or 3.
 
 """
+# pylint: disable=C0103
 
 from __future__ import division, print_function
-from math import sqrt, log, exp, sin, cos, tan, asin, acos, atan, hypot, pi, e, ceil, floor, factorial, fabs, degrees, radians
+import fractions
+import itertools
+from math import sqrt, log, exp, sin, cos, tan, asin, acos, atan, hypot, \
+                 pi, e, ceil, floor, factorial, fabs, degrees, radians
 import re
+
+def ifactors(n):
+    "Iterate through the factors"
+    f = 2
+    increments = itertools.chain([1,2,2], itertools.cycle([4,2,4,2,4,6,2,6]))
+    for incr in increments:
+        if f*f > n:
+            break
+        while n % f == 0:
+            yield f
+            n //= f
+        f += incr
+    if n > 1:
+        yield n
+
+
+def factors(n):
+    '''find the factors of n and return a list... very slowly
+    >>> factors(12345)
+    [3, 5, 823]
+    >>> factors(128)
+    [2, 2, 2, 2, 2, 2, 2]
+    '''
+    return list(ifactors(n))
+
 
 phi = 1.61803398875
 
@@ -43,6 +73,8 @@ def cosd(x):
     return cos(radians(x))
 
 def tand(x):
+    '''Tangent of x in degress
+    '''
     return sind(x)/cosd(x)
 
 def choose(n, k):
@@ -73,7 +105,7 @@ def workout(s):
     2
     >>> workout(r'37\\times27')
     999
-    >>> workout('{55\over6}')
+    >>> workout(r'{55\\over6}')
     9.166666666666666
     >>> workout('2^8+pi')
     259.1415926535898
@@ -82,7 +114,11 @@ def workout(s):
     >>> workout('3e^-3')
     0.14936120510359185
     >>> workout('210mm-3in')
-    379.2755905509
+    379.27559055118115
+    >>> workout('25.4mm')
+    72.0
+    >>> workout('2.5in')
+    180.0
 
     '''
 
@@ -95,17 +131,19 @@ def workout(s):
     s = s.replace(r'{', '(')
     s = s.replace(r'}', ')')
     s = s.replace('\\','')
-    s = re.sub('(\d?\d)!',r'factorial(\1)',s)
-    s = re.sub('([0-9\)])([a-z\(])',r'\1*\2',s)
-    s = re.sub('(\d+)\*mm',r'(\1*2.83464566929)',s)
-    s = re.sub('(\d+)\*in',r'(\1*72)',s)
-    s = re.sub('(\d+)\*bp',r'(\1*1)',s)
-    s = re.sub('(\d+)\*pt',r'(\1*0.996264009963)',s)
+    s = re.sub(r'(\d?\d)!',r'factorial(\1)',s)
+    s = re.sub(r'([0-9\)])([a-z\(])',r'\1*\2',s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*mm',r'(\1*720/254)',s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*in',r'(\1*72)',s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*bp',r'(\1*1)',s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*pt',r'(\1*7200/7227)',s)
     try:
-        answer = eval(s)
+        return eval(s)
     except SyntaxError:
-        answer = '['+s+']'
-    return answer
+        return '['+s+']'
+    except ValueError:
+        return '?'+s
+    return None
 
 def find_expression(line,col):
     '''Given a line and a cursor pos, return a tuple of str (prefix, expression, suffix).
@@ -124,16 +162,16 @@ def find_expression(line,col):
 
     >>> find_expression('12',1)
     ('', '12', '')
-    
+
     >>> find_expression('12 ',2)
     ('', '12', ' ')
-    
+
     >>> find_expression('12 ',2)
     ('', '12', ' ')
 
     >>> find_expression('String 3+4=',10)
     ('String ', '3+4=', '')
-    
+
 
     '''
 
@@ -144,12 +182,12 @@ def find_expression(line,col):
         return ('', '', line)
 
     alphabet = "'abcdefghijklmnopqrstuvwxyz1234567890!,.-+*/^\\{}()"
-    target = ''  
+    target = ''
     p = col
     while p>=0:
         c = line[p]
         p -= 1
-        if 0==len(target):
+        if len(target) == 0:
             if c in '='+alphabet:
                 target = c
             elif c == ' ':
@@ -164,7 +202,7 @@ def find_expression(line,col):
 
 def evaluate_expression(target):
     '''Check for terminal "signals" and call workout accordingly.
-    
+
     If the expression ends in "=" then answer is "expr = answer".
     If the expression ends in "\" then make the answer a fraction.
     Otherwise just replace the expr with the answer.
@@ -187,15 +225,14 @@ def evaluate_expression(target):
             approx = float('%g' % answer)
             rel = '=' if fabs(answer-approx)<1e-15 else '\\simeq'
             return '{0} {1} {2:g}'.format(target,rel,approx)
-        except:
+        except ValueError:
             return answer+'?'
-    
+
     if target.endswith("\\"):
-        import fractions as f
         target = target.strip("\\")
-        q = f.Fraction(workout(target)).limit_denominator()
+        q = fractions.Fraction(workout(target)).limit_denominator()
         return '{0} = {{{1.numerator}\\over{1.denominator}}}'.format(target,q)
-    
+
     return '{0}'.format(workout(target))
 
 if __name__ == '__main__':
@@ -207,7 +244,6 @@ if __name__ == '__main__':
         (prefix, expression, suffix) = find_expression(line,col)
         answer = evaluate_expression(expression)
         vim.current.line = prefix+answer+suffix+" "
-        vim.current.window.cursor = (row,len(prefix+answer)) 
+        vim.current.window.cursor = (row,len(prefix+answer))
     except ImportError:
         pass
-
