a2acbc10884cbd100111638ccf7dc32de74476ac
Toby Thurston <thurston@eml.cc>
2022-11-23 20:13:49 +0000

    Flaked8ed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-vim-proust
+
 An inline calculator for VIM
--- proust.py
+++ proust.py
@@ -1,9 +1,10 @@
 """A TeX-aware inline calculator/expression evaluator for Vim
 
-Toby Thurston -- 2016-09-22
+Toby Thurston -- 23 Nov 2022
 
-This is how I bind this to a key in MacVim.  Since I bind this to a key used by the macmenus
-this has to be in .gvimrc and you have to nop the macmenu key first.
+This is how I bind this to a key in MacVim.  Since I bind this to a key used by
+the macmenus this has to be in .gvimrc and you have to nop the macmenu key
+first.
 
     macmenu Edit.Font.Bigger key=<nop>
     imap <D-=> <ESC>:pyf ~/python-vim-proust/proust.py<CR>i
@@ -16,27 +17,28 @@ to the command sequence to run this filter.
   :pyf ~/python-vim-proust.proust.py<CR>  run this filter
   i                                       go back into insert mode
 
-The filter is written to work with Python 2 or 3.
-
 """
 # pylint: disable=C0103
 
-from __future__ import division, print_function
 import fractions
 import itertools
 import re
-from math import sqrt, log, exp, sin, cos, tan, asin, acos, atan, hypot, comb, perm, \
-                 pi, e, ceil, floor, factorial, fabs, degrees, radians, gcd
+from math import (acos, asin, atan, atan2, ceil, comb, cos, degrees, e, exp,
+                  fabs, factorial, floor, gcd, hypot, log, perm, pi, radians,
+                  sin, sqrt, tan)
+
 
 def lcm(a, b):
-    return abs(a*b)/gcd(a, b)
+    return abs(a * b) / gcd(a, b)
+
 
 def ifactors(n):
     "Iterate through the factors"
     f = 2
-    increments = itertools.chain([1,2,2], itertools.cycle([4,2,4,2,4,6,2,6]))
+    f_cycle = itertools.cycle([4, 2, 4, 2, 4, 6, 2, 6])
+    increments = itertools.chain([1, 2, 2], f_cycle)
     for incr in increments:
-        if f*f > n:
+        if f * f > n:
             break
         while n % f == 0:
             yield f
@@ -58,6 +60,7 @@ def factors(n):
 
 phi = 1.61803398875
 
+
 def sind(x):
     """
     Sine of x in degrees.
@@ -67,6 +70,7 @@ def sind(x):
     """
     return sin(radians(x))
 
+
 def cosd(x):
     '''Cosine of x in degrees
 
@@ -75,20 +79,80 @@ def cosd(x):
     '''
     return cos(radians(x))
 
+
 def tand(x):
     '''Tangent of x in degress
     '''
-    return sind(x)/cosd(x)
+    return sind(x) / cosd(x)
+
+
+def angle(a, b):
+    '''Like MP
+    >>> angle(4,3)
+    36.8699
+    '''
+    return round(degrees(atan2(b, a)), 5)
+
+
+def dir(t):
+    '''from MP
+    >>> dir(0)
+    (1.0, 0.0)
+    >>> dir(45)
+    (0.7071, 0.7071)
+    '''
+    return (round(cosd(t), 4), round(sind(t), 4))
+
+
+def mexp(x):
+    '''from MP = exp(x/256)
+    >>> mexp(0)
+    1.0
+    >>> mexp(256)
+    2.718281828459045
+    '''
+    return exp(x / 256)
+
 
 def mlog(x):
-    '''Like MP
+    ''' from MP = 256 log(x)
+    >>> mlog(10)
+    589.4617838064758
     '''
     return 256 * log(x)
 
-def mexp(x):
-    '''Also like MP
+
+def abs(a, b=0):
+    '''Like MP
+    >>> abs(3,4)
+    5.0
+    >>> abs(1)
+    1.0
+    >>> abs(-42.1)
+    42.1
     '''
-    return exp(x / 256)
+    return hypot(a, b)
+
+
+def choose(n, k):
+    '''A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
+
+    >>> choose(4,2)
+    6
+    >>> choose(2,4)
+    0
+    >>> choose(20,8)
+    125970
+    '''
+    if 0 <= k <= n:
+        ntok = 1
+        ktok = 1
+        for t in range(1, min(k, n - k) + 1):
+            ntok *= n
+            ktok *= t
+            n -= 1
+        return ntok // ktok
+    return 0
 
 
 def workout(s):
@@ -116,31 +180,33 @@ def workout(s):
 
     '''
 
-    s = s.replace(r'\times','*')
-    s = s.replace(r'\left(','(')
-    s = s.replace(r'\right)',')')
+    s = s.replace(r'\times', '*')
+    s = s.replace(r'\left(', '(')
+    s = s.replace(r'\right)', ')')
     s = re.sub(r'{(.*?)\\over(.*?)}', r'((\1)/(\2))', s)
     s = re.sub(r'{(.*?)\\choose(.*?)}', r'choose(\1,\2)', s)
-    s = s.replace(r'^','**')
+    s = s.replace(r'^', '**')
     s = s.replace(r'{', '(')
     s = s.replace(r'}', ')')
-    s = s.replace('\\','')
-    s = re.sub(r'(\d?\d)!',r'factorial(\1)',s)
-    s = re.sub(r'([0-9\)])([a-z\(])',r'\1*\2',s)
-    s = re.sub(r'(\d+(:?\.\d+)?)\*mm',r'(\1*720/254)',s)
-    s = re.sub(r'(\d+(:?\.\d+)?)\*in',r'(\1*72)',s)
-    s = re.sub(r'(\d+(:?\.\d+)?)\*bp',r'(\1*1)',s)
-    s = re.sub(r'(\d+(:?\.\d+)?)\*pt',r'(\1*7200/7227)',s)
+    s = s.replace('\\', '')
+    s = re.sub(r'(\d?\d)!', r'factorial(\1)', s)
+    s = re.sub(r'([0-9\)])([a-z\(])', r'\1*\2', s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*mm', r'(\1*720/254)', s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*in', r'(\1*72)', s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*bp', r'(\1*1)', s)
+    s = re.sub(r'(\d+(:?\.\d+)?)\*pt', r'(\1*7200/7227)', s)
     try:
         return eval(s)
     except SyntaxError:
-        return '['+s+']'
+        return '[' + s + ']'
     except ValueError:
-        return '?'+s
+        return '?' + s
     return None
 
-def find_expression(line,col):
-    '''Given a line and a cursor pos, return a tuple of str (prefix, expression, suffix).
+
+def find_expression(line, col):
+    '''Given a line and a cursor pos, 
+       return a tuple of str (prefix, expression, suffix).
 
     Note that the cursor position is 0 indexed.
     Note trailing and leading blanks are preserved.
@@ -169,30 +235,31 @@ def find_expression(line,col):
 
     '''
 
-    if len(line)==0:
+    if len(line) == 0:
         return ('', '', '')
 
-    if col==0:
+    if col == 0:
         return ('', '', line)
 
     alphabet = "'abcdefghijklmnopqrstuvwxyz1234567890!,.-+*/^\\{}()"
     target = ''
     p = col
-    while p>=0:
+    while p >= 0:
         c = line[p]
         p -= 1
         if len(target) == 0:
-            if c in '='+alphabet:
+            if c in '=' + alphabet:
                 target = c
             elif c == ' ':
                 col -= 1
         elif c in alphabet:
             target = c + target
         else:
-            p = p+1
+            p = p + 1
             break
 
-    return (line[0:p+1], target, line[col+1:])
+    return (line[0:p + 1], target, line[col + 1:])
+
 
 def evaluate_expression(target):
     '''Check for terminal "signals" and call workout accordingly.
@@ -217,27 +284,28 @@ def evaluate_expression(target):
         answer = workout(target)
         try:
             approx = float('%g' % answer)
-            rel = '=' if fabs(answer-approx)<1e-15 else '\\simeq'
-            return '{0} {1} {2:g}'.format(target,rel,approx)
+            rel = '=' if fabs(answer - approx) < 1e-15 else '\\simeq'
+            return f'{target} {rel} {approx:g}'
         except ValueError:
-            return answer+'?'
+            return answer + '?'
 
     if target.endswith("\\"):
         target = target.strip("\\")
         q = fractions.Fraction(workout(target)).limit_denominator()
-        return '{0} = {{{1.numerator}\\over{1.denominator}}}'.format(target,q)
+        return f'{target} = {{{q.numerator}\\over{q.denominator}}}'
 
     return '{0}'.format(workout(target))
+
 
 if __name__ == '__main__':
     try:
         import vim
         line = vim.current.line
-        (row,col) = vim.current.window.cursor
+        (row, col) = vim.current.window.cursor
 
-        (prefix, expression, suffix) = find_expression(line,col)
+        (prefix, expression, suffix) = find_expression(line, col)
         answer = evaluate_expression(expression)
-        vim.current.line = prefix+answer+suffix+" "
-        vim.current.window.cursor = (row,len(prefix+answer))
+        vim.current.line = prefix + answer + suffix + " "
+        vim.current.window.cursor = (row, len(prefix + answer))
     except ImportError:
         pass
